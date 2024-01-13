0bc15232dcd371dcc135feef14850ba24fea2f69
elgeneee <eedren@gmail.com>
2024-01-13 12:48:45 +0800

    fix:set enddate to december 2023

--- src/jobs/monthlyReportEmailJob.ts
+++ src/jobs/monthlyReportEmailJob.ts
@@ -70,8 +70,9 @@ export const monthlyReportEmailJob = inngest.createFunction(
     startDate.setDate(1);
 
     const endDate = new Date();
-    // endDate.setMonth(endDate.getMonth());
-    endDate.setDate(0);
+    endDate.setFullYear(2023);
+    endDate.setMonth(11);
+    endDate.setDate(31);
 
     // Fetch all users
     const users = await step.run("fetch-users", async () => {
